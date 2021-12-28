f0fa10f789ce2d6e3c47e91fc211d0a9f082ba55
Surabordin <bluelimec@gmail.com>
2021-12-28 11:46:02 +0700

    complete

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,33 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool a[][M]){
+for(int i=0;i<72;i++){
+    cout << "-";
+}
+cout << "\n";
+for(int i=0; i<N;i++){
+    cout<<"|";
+      for (int j=0;j<M;j++){
+       if ( a[i][j] < 1) cout << " ";
+       if ( a[i][j] >= 1) cout << "*";
+      }
+      cout<<"|";
+      cout << "\n";
+      }
+    for(int i=0;i<72;i++){
+    cout << "-";
+}
+cout << "\n" ;
+}
+
+void updateImage(bool a[][M],int s,int x,int y){
+    for(int i=0;i<N;i++){
+        for(int j=0;j<M;j++){
+                if(sqrt((pow(i-x,2)+(pow(j-y,2)))) +1 <= s){
+                a[i][j] = 1;
+                }
+            }
+        }
+    }
